6020785b5e3dbe27bdebffde5bcb680c695cffaf
*0iGWgdvTWg <waiyan.toshima12@gmail.com>
2025-11-28 09:47:16 +0900

    dashboard-separate-admin-logout

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth as Auth;
 
 class AdminController extends Controller
 {
     public function AdminDashboard(Request $request)
     {
-       return view('admin.admin_dashboard');
+       return view('admin.index');
         // die();
+    }
+
+    public function AdminLogout(Request $request)
+    {
+        Auth::guard('web')->logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/login');
+        // echo 'Logout Successfully';die();
     }
 }
