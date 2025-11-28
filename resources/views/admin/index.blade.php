aa48a98a109302b727ad2e4646e90a69b600feea
*0iGWgdvTWg <waiyan.toshima12@gmail.com>
2025-11-28 14:59:13 +0900

    refresh_admin_template

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -24,7 +24,7 @@
                 </button>
             </div>
         </div>
-{{-- card 1 --}}
+        {{-- card 1 --}}
         <div class="row">
             <div class="col-12 col-xl-12 stretch-card">
                 <div class="row flex-grow-1">
@@ -168,9 +168,12 @@
                     </div>
                 </div>
             </div>
-        </div> <!-- row -->
-{{-- card 2 --}}
-        <div class="row">
+        </div>
+        <!-- row -->
+        {{-- card 2 --}}
+
+        {{-- revenue card --}}
+        {{-- <div class="row">
             <div class="col-12 col-xl-12 grid-margin stretch-card">
                 <div class="card overflow-hidden">
                     <div class="card-body">
@@ -219,10 +222,12 @@
                     </div>
                 </div>
             </div>
-        </div> <!-- row -->
-{{-- card 3 --}}
+        </div> --}}
+        <!-- row -->
+
+        {{-- card 3 --}}
         <div class="row">
-            <div class="col-lg-7 col-xl-8 grid-margin stretch-card">
+            <div class="col-lg-12 col-xl-12 grid-margin stretch-card">
                 <div class="card">
                     <div class="card-body">
                         <div class="d-flex justify-content-between align-items-baseline mb-2">
@@ -257,7 +262,8 @@
                     </div>
                 </div>
             </div>
-            <div class="col-lg-5 col-xl-4 grid-margin stretch-card">
+            {{-- remove cloud storage --}}
+            {{-- <div class="col-lg-5 col-xl-4 grid-margin stretch-card">
                 <div class="card">
                     <div class="card-body">
                         <div class="d-flex justify-content-between align-items-baseline">
@@ -310,9 +316,11 @@
                         </div>
                     </div>
                 </div>
-            </div>
-        </div> <!-- row -->
-{{-- card 4 --}}
+            </div> --}}
+        </div>
+        <!-- row -->
+
+        {{-- card 4 --}}
         <div class="row">
             <div class="col-lg-5 col-xl-4 grid-margin grid-margin-xl-0 stretch-card">
                 <div class="card">
